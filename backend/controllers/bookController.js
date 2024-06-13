40bee095396d1d21e00198afe57542f0d9ab145b
Ethan Cobb <ethancobb116@gmail.com>
2024-06-13 12:41:57 +0100

    update 3

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -44,7 +44,16 @@ const createBook = async (req, res) => {
 
   // create new todo object with model
   const bookObj = new Book({
+<<<<<<< Updated upstream
     text,
+=======
+    id: id,
+    title: title,
+    author: author,
+    year: year,
+    pages: pages,
+    review: reviews,
+>>>>>>> Stashed changes
   });
   // await for it to be saved
   const newBook = await bookObj.save();
@@ -57,6 +66,7 @@ const editBook = async (req, res) => {
   // get id from ':id' param from the route
   const { id } = req.params;
   //get updated todo data from the request body
+<<<<<<< Updated upstream
   const book = await Book.findByIdAndUpdate(is, { text: req.body.text });
   res.status(200).json(book);
   // use mongoose model method findByIdAndUpdate
@@ -67,6 +77,48 @@ const deleteBook = async (req, res) => {
   const { id } = req.params;
 
   const book = await Book.findByIdAndDelete(id);
+=======
+  const { title, author, releaseDate, numberOfPages, reviews } = req.body;
+
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(
+      id,
+      {
+        title: title,
+        author: author,
+        releaseDate: releaseDate,
+        numberOfPages: numberOfPages,
+        reviews: reviews,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!updatedBook)
+      return res.status(404).json({ message: "Book not found" });
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    console.error("Error updating book", error);
+  }
+};
+
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+  console.log(id, "delete book");
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    console.log(id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Book deleted successfully", id: book._id.toString() });
+  } catch (error) {
+    console.error("Error deleting book", error);
+    res.status(500).json({ message: "Internal error: " });
+  }
+>>>>>>> Stashed changes
 };
 
 module.exports = {
@@ -74,5 +126,9 @@ module.exports = {
   getBooks,
   editBook,
   deleteBook,
+<<<<<<< Updated upstream
   getBook,
+=======
+  findBooksByAuthor,
+>>>>>>> Stashed changes
 };
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+<<<<<<< Updated upstream
   name: {
+=======
+  title: {
+>>>>>>> Stashed changes
     type: String,
     required: true,
   },
@@ -10,6 +14,7 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
+<<<<<<< Updated upstream
   releaseDate: {
     type: Date,
     required: true,
@@ -18,6 +23,15 @@ const bookSchema = new Schema({
     type: Number,
     required: true,
   },
+=======
+  year: {
+    type: Date,
+  },
+  numOfPages: {
+    type: Number,
+  },
+
+>>>>>>> Stashed changes
   reviews: [
     {
       type: Schema.Types.ObjectId,
