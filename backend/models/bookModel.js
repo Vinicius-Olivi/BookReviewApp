9789287c90e3bcd9d6f18130c52003377b7039d1
Ethan Cobb <ethancobb116@gmail.com>
2024-06-13 12:41:25 +0100

    update

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-<<<<<<< Updated upstream
   name: {
-=======
-  title: {
->>>>>>> Stashed changes
     type: String,
     required: true,
   },
@@ -14,24 +10,14 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
-<<<<<<< Updated upstream
   releaseDate: {
     type: Date,
     required: true,
-=======
-  year: {
-    type: Date,
->>>>>>> Stashed changes
   },
   numOfPages: {
     type: Number,
-<<<<<<< Updated upstream
     required: true,
   },
-=======
-  },
-
->>>>>>> Stashed changes
   reviews: [
     {
       type: Schema.Types.ObjectId,
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import SearchAndBookList from "./pages/SearchAndBookList";
+import Welcome from "./pages/Welcome";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/search" element={<SearchAndBookList />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
