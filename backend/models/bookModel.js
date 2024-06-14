5c6c98cae659c7f955bbafa56862a2b8fe8f4c5d
Vinicius Olivi <vinicius.olivi@al.infnet.edu.br>
2024-06-14 10:41:56 +0100

    last commit

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -25,33 +25,3 @@ const bookSchema = new Schema({
 });
 
 module.exports = mongoose.model("Book", bookSchema);
-
-/* const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const bookSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: Date,
-  },
-  pages: {
-    type: Number,
-    required: true,
-  },
-
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Review",
-    },
-  ],
-});
-
-module.exports = mongoose.model("Book", bookSchema); */
--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -7,21 +7,3 @@ router.post("/reviews", addReview);
 router.delete("/reviews/:reviewId", deleteReview);
 
 module.exports = router;
-
-/* const express = require("express");
-const router = express.Router();
-const reviewsController = require("../controllers/reviewController");
-
-router.get("/reviews", reviewsController.getReviews);
-router.get("/review/:id", reviewsController.getReview);
-// Route for creating a new review
-router.post("/review/:id", reviewsController.createReview);
-
-// Route for updating an existing review
-// router.patch("/reviews/:reviewId", reviewsController.updateReview);
-
-// Route for deleting a review
-// router.delete("/reviews/:reviewId", reviewsController.deleteReview);
-
-module.exports = router;
- */
